63e0258dfddb31a0ecb6a7dacc97ffb0ab795142
Davi Aravechia <alferdavi@gmail.com>
2026-09-15 21:41:17 -0300

    Criando divisorias visuais para o header, sizebar e container

---
+++ sistema-de-doacoes/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div id="app-root">
+
+      <header><h1>Header</h1></header>
+      <sizebar><h1>Sizebar</h1></sizebar>
+      <div className='container'><h1>Container</h1></div>
+    </div>
+  )
+}
+
+export default App
